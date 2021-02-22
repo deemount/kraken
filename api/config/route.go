131b074d1bd7c42290ff81c2397248c05377c8cb
deemount <salvatore.gonda@web.de>
2021-02-22 14:02:25 +0100

    Added package comments - everywhere

--- api/config/route.go
+++ api/config/route.go
@@ -1,3 +1,8 @@
+// Copyright 2021. All rights reserved.
+// Use of this source code is governed by a Apache 2.0
+// license that can be found in the LICENSE file.
+
+// Package config ...
 package config
 
 import "net/http"
